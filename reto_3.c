4b693fe0a2b84e3c70e17cbc5a9e92c6168cee3a
Dario <darioguerragarcia.lr@gmail.com>
2026-02-21 00:50:54 +0100

    Finalizando Reto_3

--- reto_3.c
+++ reto_3.c
@@ -24,7 +24,7 @@ int existeEnVector(int num, int *vectNum, int size) // CHECK
     return (0);
 }
 
-int main(void)
+/*int main(void)
 {
     int ejemplo[] = {5,4, 13, 9, 13, 0, 890}; //7
     int size = sizeof(ejemplo) / sizeof(ejemplo[0]);
@@ -42,67 +42,66 @@ int main(void)
         printf("El numero no está en el vector");
 
     return (0);
-}
+}*/
 
 
 
-/*int *interseccionVectores(int *v1, int *v2, int tamagno)
+int *interseccionVectores(int *v1, int *v2, int *v3, int size_v1, int size_v2, int *size_v3)
 {
-    int v3[tamagno];
-    int *p_v3 = v3;
     int i, j, k;
-    int sizev1 = sizeof(&v1) / sizeof(v1[0]);
-    int sizev2 = sizeof(&v2) / sizeof(v2[0]);
-
-    if (sizev1 > sizev2)
-        tamagno = sizev1;
-    else
-        tamagno = sizev2;
     
-    for (i = 0; i < sizev1; i++)
+    for (i = 0; i < size_v1; i++)
     {
-        for (j = 0; j < sizev2; j++)
+        for (j = 0; j < size_v2; j++)
         {
+            printf("\nSe está comparando %d con %d\n", v1[i], v2[j]);
             if (v1[i] == v2[j])
             {
-                if (!existeEnVector(v1[i], v3))
+                if (existeEnVector(v1[i], v3, *size_v3) == 0)
                 {
-                    v3[i] = v1[i];
-                    break;
+                    for (k = 0; k < *size_v3; k++)
+                    {
+                        printf("%d No está en V3.\n", v1[i]);
+                        v3[k] = v1[i];
+                        break;
+                    }
                 }
+                else
+                    printf("%d está en V3 y no se copia.\n", v1[i]);
+                break;
             }
         }
         j = 0;
     }
-    return (*v3);
-}*/
-/*
+    return (v3);
+}
+
 int main(void)
 {
-    int i, size, vectorMayor;
+    int i, size, sizev3, limite;
     int vector1[] = {5, 6, 7, 7, 9, 10, 9, 3, 13};
     int vector2[] = {1, 2, 7, 8, 13, 9, 3, 13};
-    int sizev1 = sizeof(&vector1) / sizeof(vector1[0]);
-    int sizev2 = sizeof(&vector2) / sizeof(vector2[0]);
-    int *p_vector1 = vector1;
-    int *p_vector2 = vector2;
+    int sizev1 = sizeof(vector1) / sizeof(vector1[0]);
+    int sizev2 = sizeof(vector2) / sizeof(vector2[0]);
+   // int *p_vector1 = vector1; //Que diferencia hay entre usar puntero o usar vector1?
+    //int *p_vector2 = vector2;
+  if (sizev1 > sizev2)
+    sizev3 = sizev1;
+  else
+    sizev3 = sizev2;
+
+    int vector3[sizev3];
+    limite = 0;
+
+
+    //Los que se repiten en ambos son 7, 9, 3, 13
+    interseccionVectores(vector1, vector2, vector3, sizev1, sizev2, &limite);
     
-    if (sizev1 > sizev2)
-          vectorMayor = sizev1;
-    else
-        vectorMayor = sizev2;
-    
-        int vector3[vectorMayor];
-      //Los que se repiten en ambos son 7, 9, 13, 3
-    
-    *vector3 = *interseccionVectores(vector1, vector2, vectorMayor);
-    
-    size = sizeof(&vector3) / sizeof(vector3[0]);
     printf("\nLos numeros que se repiten son:\n");
-    for (i = 0; i < size; i++)
+    for (i = 0; i < sizev3; i++)
     {
         printf("\n%d\n", vector3[i]);
     }
     
     return (0);
-}*/
+}
